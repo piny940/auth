69db22ace4747d25ed810027cc22093c781e9317
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-23 16:01:50 +0900

    approval_scopesはscope_idとapproval_idの両方をprimary keyにする

--- atlas/migrations/20241022031455_create_approvals.sql
+++
@@ -1,8 +0,0 @@
--- Create "approvals" table
-CREATE TABLE "public"."approvals" ("id" bigserial NOT NULL, "client_id" character varying(16) NOT NULL, "user_id" bigint NOT NULL, "created_at" timestamptz NOT NULL, "updated_at" timestamptz NOT NULL, PRIMARY KEY ("id"), CONSTRAINT "client_id" FOREIGN KEY ("client_id") REFERENCES "public"."clients" ("id") ON UPDATE NO ACTION ON DELETE CASCADE, CONSTRAINT "user_id" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON UPDATE NO ACTION ON DELETE CASCADE);
--- Create index "user_id_client_id" to table: "approvals"
-CREATE UNIQUE INDEX "user_id_client_id" ON "public"."approvals" ("user_id", "client_id");
--- Create "approval_scopes" table
-CREATE TABLE "public"."approval_scopes" ("id" integer NOT NULL, "approval_id" bigint NOT NULL, PRIMARY KEY ("id"), CONSTRAINT "approval_id" FOREIGN KEY ("approval_id") REFERENCES "public"."approvals" ("id") ON UPDATE NO ACTION ON DELETE CASCADE);
--- Create index "approval_id" to table: "approval_scopes"
-CREATE INDEX "approval_id" ON "public"."approval_scopes" ("approval_id");
