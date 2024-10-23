ae45cf30db2c096877b149e1c3f7a89bf3d6c096
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-23 16:08:31 +0900

    approvalsのsqlを作成

---
+++ atlas/migrations/20241023070357.sql
@@ -0,0 +1,6 @@
+-- Create "approvals" table
+CREATE TABLE "public"."approvals" ("id" bigserial NOT NULL, "client_id" character varying(16) NOT NULL, "user_id" bigint NOT NULL, "created_at" timestamptz NOT NULL, "updated_at" timestamptz NOT NULL, PRIMARY KEY ("id"), CONSTRAINT "client_id" FOREIGN KEY ("client_id") REFERENCES "public"."clients" ("id") ON UPDATE NO ACTION ON DELETE CASCADE, CONSTRAINT "user_id" FOREIGN KEY ("user_id") REFERENCES "public"."users" ("id") ON UPDATE NO ACTION ON DELETE CASCADE);
+-- Create index "user_id_client_id" to table: "approvals"
+CREATE UNIQUE INDEX "user_id_client_id" ON "public"."approvals" ("user_id", "client_id");
+-- Create "approval_scopes" table
+CREATE TABLE "public"."approval_scopes" ("scope_id" integer NOT NULL, "approval_id" bigint NOT NULL, PRIMARY KEY ("scope_id", "approval_id"), CONSTRAINT "approval_id" FOREIGN KEY ("approval_id") REFERENCES "public"."approvals" ("id") ON UPDATE NO ACTION ON DELETE CASCADE);
