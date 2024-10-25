4a9a72c7b34e7859c1e2ad0da0c89ddedd7e3bcd
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-25 12:50:27 +0900

    Client.SecretCorrectを作成

--- internal/domain/oauth/client.go
+++ internal/domain/oauth/client.go
@@ -1,6 +1,11 @@
 package oauth
 
-import "time"
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type ClientID string
 type Client struct {
@@ -14,4 +19,23 @@ type Client struct {
 }
 type IClientRepo interface {
 	FindByID(id ClientID) (*Client, error)
+}
+
+func (c *Client) SecretCorrect(secret string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(c.EncryptedSecret), []byte(secret))
+	if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func EncryptClientSecret(raw string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(raw), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
 }
---
+++ internal/domain/oauth/client_test.go
@@ -0,0 +1,63 @@
+package oauth
+
+import (
+	"errors"
+	"testing"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestEncryptClientSecret(t *testing.T) {
+	suites := []struct {
+		Name      string
+		Secret    string
+		ToCompare string
+		Correct   bool
+	}{
+		{"correct secret", "test_secret", "test_secret", true},
+		{"invalid secret", "test_secret, test_secret", "test_secret", false},
+	}
+	for _, suit := range suites {
+		t.Run(suit.Name, func(t *testing.T) {
+			hash, err := EncryptClientSecret(suit.Secret)
+			if err != nil {
+				t.Fatal(err)
+			}
+			err = bcrypt.CompareHashAndPassword([]byte(hash), []byte(suit.ToCompare))
+			if err != nil && !errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+				t.Fatal(err)
+			}
+			actualIncorrect := errors.Is(err, bcrypt.ErrMismatchedHashAndPassword)
+			if actualIncorrect != !suit.Correct {
+				t.Errorf("unexpected result: %v", actualIncorrect)
+			}
+		})
+	}
+}
+
+func TestClientCorrectSecret(t *testing.T) {
+	secret := "test_secret"
+	suites := []struct {
+		Name      string
+		ToCompare string
+	}{
+		{"correct secret", secret},
+		{"invalid secret", "invalid_secret"},
+	}
+	for _, suit := range suites {
+		t.Run(suit.Name, func(t *testing.T) {
+			hash, err := bcrypt.GenerateFromPassword([]byte(secret), bcrypt.DefaultCost)
+			if err != nil {
+				t.Fatal(err)
+			}
+			client := &Client{EncryptedSecret: string(hash)}
+			correct, err := client.SecretCorrect(suit.ToCompare)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if correct != (suit.ToCompare == secret) {
+				t.Errorf("unexpected result: %v", correct)
+			}
+		})
+	}
+}
