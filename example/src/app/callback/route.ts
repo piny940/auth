d7aff8f2ee744cf8a9df52d5e1f23b1b9ce65d8a
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-27 20:41:32 +0900

    authにリダイレクトさせるようにする

---
+++ example/src/app/callback/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest } from "next/server"
+import path from "path"
+
+export async function GET(request: NextRequest) {
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+    throw new Error("NEXT_PUBLIC_API_URL is not set")
+  }
+  const code = request.nextUrl.searchParams.get("code")
+  if (!code) {
+    throw new Error("code is not set")
+  }
+  if (!process.env.NEXT_PUBLIC_CLIENT_ID) {
+    throw new Error("NEXT_PUBLIC_CLIENT_ID is not set")
+  }
+  if (!process.env.NEXT_PUBLIC_APP_URL) {
+    throw new Error("NEXT_PUBLIC_APP_URL is not set")
+  }
+  if (!process.env.CLIENT_SECRET) {
+    throw new Error("CLIENT_SECRET is not set")
+  }
+  const secret = Buffer.from(
+    `${process.env.NEXT_PUBLIC_CLIENT_ID}:${process.env.CLIENT_SECRET}}`
+  )
+  const res = await fetch(
+    path.join(process.env.NEXT_PUBLIC_API_URL, "oauth", "token"),
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${secret.toString("base64")}`,
+      },
+      body: new URLSearchParams({
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        code: code,
+        grant_type: "authorization_code",
+        redirect_uri: path.join(process.env.NEXT_PUBLIC_APP_URL, "callback"),
+      }),
+    }
+  )
+  const json = await res.json()
+  console.log(json)
+  return new Response()
+}
--- example/src/app/page.tsx
+++ example/src/app/page.tsx
@@ -1,3 +1,24 @@
+import Link from "next/link"
+import path from "path"
+
 export default function Home() {
-  return <main>ほげ</main>
+  if (!process.env.NEXT_PUBLIC_APP_URL) {
+    throw new Error("NEXT_PUBLIC_APP_URL is not set")
+  }
+  if (!process.env.NEXT_PUBLIC_API_URL) {
+    throw new Error("NEXT_PUBLIC_API_URL is not set")
+  }
+  const redirectUri = path.join(process.env.NEXT_PUBLIC_APP_URL, "callback")
+  const link = `${process.env.NEXT_PUBLIC_API_URL}/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}&response_type=code&redirect_uri=${redirectUri}&scope=openid`
+  return (
+    <main className="m-20">
+      <h1 className="text-4xl font-bold">Example Client</h1>
+      <Link
+        href={link}
+        className="inline-block m-10 bg-sky-400 rounded p-3 text-lg font-medium hover:bg-sky-500"
+      >
+        mikanでログイン
+      </Link>
+    </main>
+  )
 }
