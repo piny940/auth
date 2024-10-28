3d27bbeb17255eee9035d6a9134c6bb0ff56bc77
piny940 <83708535+piny940@users.noreply.github.com>
2024-10-28 11:48:08 +0900

    api urlのenv名を修正

--- frontend/src/utils/client.ts
+++ frontend/src/utils/client.ts
@@ -2,6 +2,6 @@ import createClient from 'openapi-fetch'
 import { paths } from './api'
 
 export const client = createClient<paths>({
-  baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
   credentials: 'include',
 })
