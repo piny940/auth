3b48330c52f92c5f1dd66e6134720d519ea0bf48
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-04 19:57:09 +0900

    ValidScopesのテストを追加

--- internal/infrastructure/gateway/scope.go
+++ internal/infrastructure/gateway/scope.go
@@ -4,7 +4,9 @@ import "auth/internal/domain/oauth"
 
 var scopeMap = map[int32]oauth.TypeScope{
 	0: oauth.ScopeOpenID,
+	1: oauth.ScopeEmail,
 }
 var scopeMapReverse = map[oauth.TypeScope]int32{
 	oauth.ScopeOpenID: 0,
+	oauth.ScopeEmail:  1,
 }
