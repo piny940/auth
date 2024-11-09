8f96b43490856fc719db5b560b0684685703abc6
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-10 00:11:35 +0900

    token requestのテストを追加

--- test/e2e/oauth_test.go
+++ test/e2e/oauth_test.go
@@ -1,21 +1,26 @@
 package e2e
 
 import (
+	"auth/internal/api"
 	"auth/internal/domain/oauth"
 	"auth/internal/infrastructure/gateway"
 	"auth/internal/infrastructure/model"
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/lestrrat-go/jwx/jwk"
 	"golang.org/x/crypto/bcrypt"
 )
 
-func TestAuthorizeCode(t *testing.T) {
+func TestOAuthAuthorize(t *testing.T) {
 	const userID = 43234
 	const username = "user1"
 	const password = "password"
@@ -88,7 +93,7 @@ func TestAuthorizeCode(t *testing.T) {
 		t.Run(suit.name, func(t *testing.T) {
 			s := newServer(t)
 			defer s.Close()
-			seed(t, initialUsers, initialClients, initialRedirectURIs, initialApprovals, initialApprovalScopes)
+			seed(t, initialUsers, initialClients, initialRedirectURIs, initialApprovals, initialApprovalScopes, nil, nil)
 
 			var cookie *string
 			if suit.authenticated {
@@ -169,7 +174,174 @@ func TestAuthorizeCode(t *testing.T) {
 	}
 }
 
-func TestJwks(t *testing.T) {
+func TestOAuthToken(t *testing.T) {
+	const userID = 43234
+	const username = "user1"
+	const clientOwnerID = 32478
+	const password = "password"
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		t.Fatalf("failed to hash password: %v", err)
+	}
+	initialUsers := []*model.User{
+		{ID: userID, Name: username, EncryptedPassword: string(hashed)},
+		{ID: clientOwnerID, Name: "client owner", EncryptedPassword: "password"},
+	}
+	const client1ID = "client1"
+	const client2ID = "client2"
+	const clientSecret = "secret"
+	hashed, err = bcrypt.GenerateFromPassword([]byte(clientSecret), bcrypt.DefaultCost)
+	if err != nil {
+		t.Fatalf("failed to hash password: %v", err)
+	}
+	initialClients := []*model.Client{
+		{ID: client1ID, Name: "approved", EncryptedSecret: string(hashed), UserID: clientOwnerID},
+		{ID: client2ID, Name: "auth code not issued", EncryptedSecret: string(hashed), UserID: clientOwnerID},
+	}
+	authCodeIds := []int64{3423, 32424, 32423, 2313, 3245}
+	const validCode = "validCode"
+	const withoutOpenid = "withoutOpenid"
+	const emptyScope = "emptyScope"
+	const usedCode = "usedCode"
+	const expiredCode = "expiredCode"
+	const validRedirectURI = "https://example.com/callback"
+	expiresAt := time.Now().Add(time.Hour)
+	initialAuthCodes := []*model.AuthCode{
+		{ID: authCodeIds[0], Value: validCode, RedirectURI: validRedirectURI, Used: false, ExpiresAt: expiresAt, ClientID: client1ID, UserID: userID},
+		{ID: authCodeIds[1], Value: usedCode, RedirectURI: validRedirectURI, Used: true, ExpiresAt: expiresAt, ClientID: client1ID, UserID: userID},
+		{ID: authCodeIds[2], Value: expiredCode, RedirectURI: validRedirectURI, Used: false, ExpiresAt: time.Now().Add(-time.Hour), ClientID: client1ID, UserID: userID},
+		{ID: authCodeIds[3], Value: withoutOpenid, RedirectURI: validRedirectURI, Used: false, ExpiresAt: expiresAt, ClientID: client2ID, UserID: userID},
+		{ID: authCodeIds[4], Value: emptyScope, RedirectURI: validRedirectURI, Used: false, ExpiresAt: expiresAt, ClientID: client1ID, UserID: userID},
+	}
+	initialAuthCodeScopes := []*model.AuthCodeScope{
+		{AuthCodeID: authCodeIds[0], ScopeID: gateway.ScopeMapReverse[oauth.ScopeOpenID]},
+		{AuthCodeID: authCodeIds[0], ScopeID: gateway.ScopeMapReverse[oauth.ScopeEmail]},
+		{AuthCodeID: authCodeIds[3], ScopeID: gateway.ScopeMapReverse[oauth.ScopeEmail]},
+	}
+
+	suites := []struct {
+		name              string
+		grantType         api.OAuthTokenGrantType
+		code              string
+		redirectURI       string
+		clientId          string
+		clientSecret      string
+		expected          int
+		idTokenIssued     bool
+		accessTokenIssued bool
+	}{
+		{"invalid grant type", "invalid", validCode, validRedirectURI, client1ID, clientSecret, 400, false, false},
+		{"code not found", api.AuthorizationCode, "invalid", validRedirectURI, client1ID, clientSecret, 400, false, false},
+		{"code used", api.AuthorizationCode, usedCode, validRedirectURI, client1ID, clientSecret, 400, false, false},
+		{"code expired", api.AuthorizationCode, expiredCode, validRedirectURI, client1ID, clientSecret, 400, false, false},
+		{"redirect uri not match", api.AuthorizationCode, validCode, "https://example.com/invalid", client1ID, clientSecret, 400, false, false},
+		{"client not found", api.AuthorizationCode, validCode, validRedirectURI, "invalid", clientSecret, 403, false, false},
+		{"client secret not match", api.AuthorizationCode, validCode, validRedirectURI, client1ID, "invalid", 403, false, false},
+		{"success", api.AuthorizationCode, validCode, validRedirectURI, client1ID, clientSecret, 200, true, true},
+		{"without openid", api.AuthorizationCode, withoutOpenid, validRedirectURI, client2ID, clientSecret, 200, false, true},
+		{"empty scope", api.AuthorizationCode, emptyScope, validRedirectURI, client1ID, clientSecret, 200, false, true},
+	}
+
+	for _, suit := range suites {
+		t.Run(suit.name, func(t *testing.T) {
+			s := newServer(t)
+			defer s.Close()
+
+			seed(t, initialUsers, initialClients, nil, nil, nil, initialAuthCodes, initialAuthCodeScopes)
+
+			form := url.Values{}
+			form.Add("grant_type", string(suit.grantType))
+			form.Add("code", suit.code)
+			form.Add("redirect_uri", suit.redirectURI)
+			form.Add("client_id", suit.clientId)
+			reqBody := strings.NewReader(form.Encode())
+			req, err := http.NewRequest(http.MethodPost, s.URL+"/oauth/token", reqBody)
+			if err != nil {
+				t.Fatalf("failed to create request: %v", err)
+			}
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			req.SetBasicAuth(suit.clientId, suit.clientSecret)
+
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("failed to send request: %v", err)
+			}
+			defer res.Body.Close()
+
+			if res.StatusCode != suit.expected {
+				body, err := io.ReadAll(res.Body)
+				if err != nil {
+					t.Fatalf("failed to read response body: %v", err)
+				}
+				t.Fatalf("expected status code: %d, but got %d. response body: %v", suit.expected, res.StatusCode, string(body))
+			}
+			if suit.expected != 200 {
+				return
+			}
+			data := &api.OAuthTokenRes{}
+			fromJSONBody(t, res.Body, data)
+			if suit.idTokenIssued {
+				if data.IdToken == nil {
+					t.Errorf("id token is not issued")
+				}
+				{ // validate id token
+					claims := parseToken(t, s, *data.IdToken).(jwt.MapClaims)
+					if claims["iss"] != os.Getenv("OAUTH_ISSUER") {
+						t.Errorf("unexpected aud: %v", claims["aud"])
+					}
+					const gap = 5 * time.Second
+					expiresAt := time.Unix(int64(claims["exp"].(float64)), 0)
+					expectedExpiresAt := time.Now().Add(oauth.ID_TOKEN_TTL)
+					if expiresAt.Before(expectedExpiresAt.Add(-gap)) || expiresAt.After(expectedExpiresAt.Add(gap)) {
+						t.Errorf("unexpected exp: %v", expiresAt)
+					}
+					if claims["sub"] != fmt.Sprintf("id:%d;name:%s", userID, username) {
+						t.Errorf("unexpected sub: %v", claims["sub"])
+					}
+					issuedAt := time.Unix(int64(claims["iat"].(float64)), 0)
+					if issuedAt.Before(time.Now().Add(-gap)) || issuedAt.After(time.Now().Add(gap)) {
+						t.Errorf("unexpected iat: %v", issuedAt)
+					}
+					if claims["jti"] == "" {
+						t.Errorf("jti is empty")
+					}
+				}
+			}
+			if suit.accessTokenIssued {
+				if data.AccessToken == "" {
+					t.Errorf("access token is not issued")
+				}
+			}
+			{ // validate access token
+				if data.AccessToken == "" {
+					t.Fatalf("access token is empty")
+				}
+				claims := parseToken(t, s, data.AccessToken).(jwt.MapClaims)
+				if claims["iss"] != os.Getenv("OAUTH_ISSUER") {
+					t.Errorf("unexpected aud: %v", claims["aud"])
+				}
+				const gap = 5 * time.Second
+				expiresAt := time.Unix(int64(claims["exp"].(float64)), 0)
+				expectedExpiresAt := time.Now().Add(oauth.ACCESS_TOKEN_TTL)
+				if expiresAt.Before(expectedExpiresAt.Add(-gap)) || expiresAt.After(expectedExpiresAt.Add(gap)) {
+					t.Errorf("unexpected exp: %v", expiresAt)
+				}
+				if claims["sub"] != fmt.Sprintf("id:%d;name:%s", userID, username) {
+					t.Errorf("unexpected sub: %v", claims["sub"])
+				}
+				issuedAt := time.Unix(int64(claims["iat"].(float64)), 0)
+				if issuedAt.Before(time.Now().Add(-gap)) || issuedAt.After(time.Now().Add(gap)) {
+					t.Errorf("unexpected iat: %v", issuedAt)
+				}
+				if claims["jti"] == "" {
+					t.Errorf("jti is empty")
+				}
+			}
+		})
+	}
+}
+
+func TestOAuthJwks(t *testing.T) {
 	s := newServer(t)
 	defer s.Close()
 
@@ -191,4 +363,44 @@ func TestJwks(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to parse jwks: %v", err)
 	}
+}
+
+func parseToken(t *testing.T, s *httptest.Server, token string) jwt.Claims {
+	t.Helper()
+
+	res, err := http.Get(s.URL + "/oauth/jwks")
+	if err != nil {
+		t.Fatalf("failed to send request: %v", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		t.Fatalf("expected status code: 200, but got %d", res.StatusCode)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("failed to read response body: %v", err)
+	}
+	set, err := jwk.Parse(body)
+	if err != nil {
+		t.Fatalf("failed to parse jwks: %v", err)
+	}
+	tokenObj, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		kid, ok := token.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("missing kid")
+		}
+		key, ok := set.LookupKeyID(kid)
+		if !ok {
+			return nil, fmt.Errorf("key not found")
+		}
+		var pubKey interface{}
+		if err := key.Raw(&pubKey); err != nil {
+			return nil, fmt.Errorf("failed to get public key: %w", err)
+		}
+		return pubKey, nil
+	})
+	if err != nil {
+		t.Fatalf("failed to parse token: %v", err)
+	}
+	return tokenObj.Claims
 }
