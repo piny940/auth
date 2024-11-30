1650888dbb13591ec1922694f4584bdfef527753
piny940 <83708535+piny940@users.noreply.github.com>
2024-11-30 22:02:11 +0900

    server sideでfetchがエラーを起こさないようにする

--- frontend/src/utils/cookie.ts
+++ frontend/src/utils/cookie.ts
@@ -1,4 +1,5 @@
 export const fromCookie = (key: string): string | undefined => {
+  if (typeof document === 'undefined') return
   const match = document.cookie.match(new RegExp('(^| )' + key + '=([^;]+)'))
   if (match) return match[2]
   return
