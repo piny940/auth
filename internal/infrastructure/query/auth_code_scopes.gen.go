838e6cc8215c101e79bc1391bbeb26456310df2f
piny940 <83708535+piny940@users.noreply.github.com>
2025-06-19 10:33:51 +0900

    go generate
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- internal/infrastructure/query/auth_code_scopes.gen.go
+++ internal/infrastructure/query/auth_code_scopes.gen.go
@@ -6,6 +6,7 @@ package query
 
 import (
 	"context"
+	"database/sql"
 
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
@@ -155,6 +156,8 @@ type IAuthCodeScopeDo interface {
 	FirstOrCreate() (*model.AuthCodeScope, error)
 	FindByPage(offset int, limit int) (result []*model.AuthCodeScope, count int64, err error)
 	ScanByPage(result interface{}, offset int, limit int) (count int64, err error)
+	Rows() (*sql.Rows, error)
+	Row() *sql.Row
 	Scan(result interface{}) (err error)
 	Returning(value interface{}, columns ...string) IAuthCodeScopeDo
 	UnderlyingDB() *gorm.DB
