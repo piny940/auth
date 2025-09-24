ee7417bbc51afaa8c7934969915b27cd1cf7cdcf
piny940 <83708535+piny940@users.noreply.github.com>
2025-09-25 00:42:01 +0900

    exampleのeslintを修正
    Signed-off-by: piny940 <83708535+piny940@users.noreply.github.com>

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -3,14 +3,21 @@ import { FlatCompat } from '@eslint/eslintrc'
 import { includeIgnoreFile } from '@eslint/compat'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import { fileURLToPath } from 'node:url'
+import path from 'node:path'
 import stylistic from '@stylistic/eslint-plugin'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 const gitignorePath = fileURLToPath(new URL('.gitignore', import.meta.url))
 
 export default defineConfig([
-  js.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   stylistic.configs.recommended,
-  new FlatCompat().extends('next/core-web-vitals', 'next/typescript'),
 
   includeIgnoreFile(gitignorePath, 'Imported .gitignore patterns'),
   globalIgnores(['*.config.*']),
